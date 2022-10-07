ddc3e2ced6614aa3214d2eadac57cfa706696e83
sayali-2001 <60540661+sayali-2001@users.noreply.github.com>
2022-10-07 14:03:00 +0530

    Linked List by adding 30 and 56 to 70

---
+++ LinkedListMain.java
@@ -0,0 +1,13 @@
+package org.Example;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        LinkedList linkedlist = new LinkedList();
+        linkedlist.addFirst(70);
+        linkedlist.display();
+        linkedlist.addFirst(30);
+        linkedlist.display();
+        linkedlist.addFirst(56);
+        linkedlist.display();
+     }
+}
